09623729ffad5bc7b174cc3e729de38abdd94ca2
Hussein Najah <husseinnajah123@gmail.com>
2023-09-13 13:56:04 +0300

    auth is done

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,5 +1,7 @@
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
-  return <div className="bg-red-200 w-full h-full">{children}</div>;
+  return (
+    <div className="h-full flex items-center justify-center">{children}</div>
+  );
 };
 
 export default AuthLayout;
