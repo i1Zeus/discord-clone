6d2822c12437193dd0e752308900c51ef34a5736
Hussein Najah <husseinnajah123@gmail.com>
2023-09-13 13:20:06 +0300

    main

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,10 +1,7 @@
-import { Button } from "@/components/ui/button";
-
 export default function Home() {
   return (
     <div>
       <p>Discord Clone</p>
-      <Button>Click me</Button>
     </div>
   );
 }
