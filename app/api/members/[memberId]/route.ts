2af3948ee97f8e95ab2ae6a1d3bf24888e8bd655
Hussein Najah <husseinnajah123@gmail.com>
2023-09-18 17:44:34 +0300

    delete member

--- app/api/members/[memberId]/route.ts
+++ app/api/members/[memberId]/route.ts
@@ -2,6 +2,55 @@ import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 import { NextResponse } from "next/server";
 
+export async function DELETE(
+  req: Request,
+  { params }: { params: { memberId: string } }
+) {
+  try {
+    const profile = await currentProfile();
+    const { searchParams } = new URL(req.url);
+
+    if (!profile) return new NextResponse("Unauthorized", { status: 401 });
+
+    const serverId = searchParams.get("serverId");
+    if (!serverId)
+      return new NextResponse("Server ID is missing", { status: 400 });
+
+    if (!params.memberId)
+      return new NextResponse("Member ID is missing", { status: 400 });
+
+    const server = await db.server.update({
+      where: {
+        id: serverId,
+        profileId: profile.id,
+      },
+      data: {
+        members: {
+          deleteMany: {
+            id: params.memberId,
+            profileId: {
+              not: profile.id,
+            },
+          },
+        },
+      },
+      include: {
+        members: {
+          include: {
+            profile: true,
+          },
+          orderBy: {
+            role: "asc",
+          },
+        },
+      },
+    });
+  } catch (error) {
+    console.log("[MEMBER_ID_DELETE]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
+
 export async function PATCH(
   req: Request,
   { params }: { params: { memberId: string } }
