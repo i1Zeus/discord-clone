6a669a03bb8e6b58d5f86e547294c9117893289e
Hussein Najah <husseinnajah123@gmail.com>
2023-09-22 00:42:29 +0300

    page now redierct to general text

--- app/(main)/(routes)/servers/[serverId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/page.tsx
@@ -1,10 +1,40 @@
-const ServerPage = () => {
-  return (
-    <div>
-      this is a server ServerPage
-      <p>Server Id Page</p>
-    </div>
-  );
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { redirectToSignIn } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+interface ServerPageProps {
+  params: {
+    serverId: string;
+  };
+}
+
+const ServerPage = async ({ params }: ServerPageProps) => {
+  const profile = await currentProfile();
+  if (!profile) return redirectToSignIn();
+
+  const server = await db.server.findUnique({
+    where: {
+      id: params.serverId,
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+    include: {
+      channels: {
+        where: {
+          name: "general",
+        },
+      },
+    },
+  });
+
+  const initialChannel = server?.channels[0];
+  if (initialChannel?.name !== "general") return null;
+
+  return redirect(`/servers/${params.serverId}/channels/${initialChannel.id}`);
 };
 
 export default ServerPage;
