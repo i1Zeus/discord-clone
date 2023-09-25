dc721a5cb0b0e184757719bef96598c915dc54a2
Hussein Najah <husseinnajah123@gmail.com>
2023-09-25 23:35:39 +0300

    query provider + hook + chat massages

--- components/chat/chat-messages.tsx
+++ components/chat/chat-messages.tsx
@@ -2,6 +2,8 @@
 
 import { Member } from "@prisma/client";
 import { ChatWelcome } from "./chat-welcome";
+import { useChatQuery } from "@/hooks/use-chat-query";
+import { Loader2, ServerCrash } from "lucide-react";
 
 interface ChatMessagesProps {
   name: string;
@@ -26,6 +28,36 @@ export const ChatMessages = ({
   paramValue,
   type,
 }: ChatMessagesProps) => {
+  const queryKey = `chat:${chatId}`;
+
+  const { data, fetchNextPage, hasNextPage, isFetchingNextPage, status } =
+    useChatQuery({
+      queryKey,
+      apiUrl,
+      paramKey,
+      paramValue,
+    });
+
+  if (status === "loading")
+    return (
+      <div className="flex-1 flex flex-col justify-center items-center">
+        <Loader2 className="w-7 h-7 text-zinc-500 animate-spin my-4" />
+        <p className="text-xs text-zinc-500 dark:text-zinc-400 capitalize">
+          Loading messages
+        </p>
+      </div>
+    );
+
+  if (status === "error")
+    return (
+      <div className="flex-1 flex flex-col justify-center items-center">
+        <ServerCrash className="w-7 h-7 text-zinc-500 my-4" />
+        <p className="text-xs text-zinc-500 dark:text-zinc-400 capitalize">
+          Something went wrong, please try refreshing the page.
+        </p>
+      </div>
+    );
+
   return (
     <div className="flex-1 flex flex-col overflow-y-auto">
       <div className="flex-1" />
